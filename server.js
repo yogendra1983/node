368715ad795ee70f8a67f6b553778c83afe22d57
Yogendra Singh <php.yogendra@gmail.com>
2018-08-26 23:03:26 +0530

    my first node application
    i am working on node js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function(){
+    console.log("Magic happens now at port 3000");
+});
+
+
+// run project // node server.js
